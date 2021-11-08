47bb0a75f792d5453d08071ad14e6ba28400e883
Марина Разумова <marina.razumova.19@yandex.ru>
2021-11-08 14:39:47 +0300

    Добавьте файлы проекта.

---
+++ Задача1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+namespace Задача1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Выберите любую папку на своем компьютере, имеющую вложенные директории. 
+             * Выведите на консоль ее содержимое и содержимое всех подкаталогов.  */
+           
+            string path = @"C:\Program Files";
+
+            string [] folders = Directory.GetDirectories(path);
+            Console.WriteLine("Папки :");
+            foreach (string d in folders)
+            {
+                Console.WriteLine(d);
+            }
+            Console.WriteLine("Файлы :");
+            string[] files = Directory.GetFiles(path);
+            foreach (string s in files)
+            {
+                Console.WriteLine(s);
+            }
+
+             Console.ReadKey();
+
+        }
+    }
+}
---
+++ Задача2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+namespace Задача2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Программно создайте текстовый файл и запишите в него 10 случайных чисел. 
+             * Затем программно откройте созданный файл, рассчитайте сумму чисел в нем, ответ выведите на консоль.  */
+
+            string path = @"C:\Users\Admin\Desktop\ИТМО\домашки\Задание8\text2.txt";
+
+            StreamWriter a = new StreamWriter(path);
+            double[] numbers = new double[10];
+            Random random = new Random();
+            double sum = 0;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = random.Next(0, 10);
+                a.WriteLine(numbers[i]);
+                sum += numbers[i];
+            }
+
+            a.Close();
+            StreamReader sr = new StreamReader(path);
+
+            Console.WriteLine(sr.ReadToEnd());
+            sr.Close();
+            Console.WriteLine("Cумма чисел : {0}", sum);
+
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
